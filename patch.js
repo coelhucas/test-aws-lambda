73a6e3709da708fa53c1ce1f460e26374abe40c8
Lucas Coelho <lucascoelhodacosta@gmail.com>
2024-08-06 11:31:51 -0300

    chore: base code

---
+++ patch.js
@@ -0,0 +1,19 @@
+const fs = require("node:fs");
+const child_process = require("node:child_process");
+
+const file = require("./version");
+
+const newVersion = file.version + 1;
+const content = `module.exports = {
+  version: ${newVersion},
+};`;
+
+try {
+  fs.writeFileSync("./version.js", content);
+
+  child_process.execSync(
+    `rm -rf dist ; mkdir dist ; zip -r ./dist/api.zip version.js`,
+  );
+} catch (err) {
+  console.error(err);
+}
